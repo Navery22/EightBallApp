a5ad2aa5805194e97ad5454de87fb62c6de021cb
Nolan <navery@zagmail.gonzaga.edu>
2022-01-25 21:33:36 -0800

    initial commit

--- language/ViewController.swift
+++ language/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var msgLabel: UILabel!
+    
+    var answers = ["Yes.", "No.", "It is Possible.", "Maybe", "If you wish", "it is Certain", "Not in a million years", "If you're good", "No, Vanessa", "If Mr.Blaustone says so", "you can count on it", "Hard to say", "Probably Not"]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        msgLabel.text = ""
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func button(_ sender: UIButton) {
+        let selectedAnswer = answers.randomElement()
+        msgLabel.text = selectedAnswer
+    }
+    
 }
-
